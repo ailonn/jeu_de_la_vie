bcd97d91be451ae5a69df5878ee3e61f1dd3c0db
Matthieu Lauze <matthieu.lauze@comwatt.com>
2020-06-22 16:04:55 +0200

    syle : cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 const MAX_ITERATION: u32 = 10;
 
-const MAX_X:usize = 25;
-const MAX_Y:usize = 100;
+const MAX_X: usize = 25;
+const MAX_Y: usize = 100;
 
-const MID_X_INDEX : usize = (MAX_X - 1)/2;
-const MID_Y_INDEX : usize = (MAX_Y - 1)/2;
+const MID_X_INDEX: usize = (MAX_X - 1) / 2;
+const MID_Y_INDEX: usize = (MAX_Y - 1) / 2;
 
-const FULL : char = 'O';
-const EMPTY : char = ' ';
+const FULL: char = 'O';
+const EMPTY: char = ' ';
 
 type Map = [[char; MAX_Y]; MAX_X];
 
@@ -25,16 +25,15 @@ type Map = [[char; MAX_Y]; MAX_X];
 
 // une entité : structure (Point ?) + Trait en fonction du type d'entité
 /* gestionnaire de vie : module avec une structure définissant les types d'entités, leur condition d'apparition et un constructeur
- leur probabilité d'apparition est géré par un Trait ? 
+ leur probabilité d'apparition est géré par un Trait ?
 */
 // pour la doc : `cargo doc`
-
 
 fn main() {
     jeu_de_la_vie();
 }
 
-fn print_the_map(world : &Map) {
+fn print_the_map(world: &Map) {
     for x_line in world.iter() {
         print!("|");
         for y_case in x_line.iter() {
@@ -45,26 +44,34 @@ fn print_the_map(world : &Map) {
 }
 
 fn if_edge_else(index: usize, limit: usize) -> usize {
-    if index == 0 {limit - 1} else {index - 1}
+    if index == 0 {
+        limit - 1
+    } else {
+        index - 1
+    }
 }
 
 fn if_limit_else(index: usize, limit: usize) -> usize {
-    if index == limit - 1 {0} else {index + 1}
+    if index == limit - 1 {
+        0
+    } else {
+        index + 1
+    }
 }
 
 //fn slice_map(world, case.perception, idx_x, idx_y) {
 
 //}
 
-fn update_the_map(world : &Map) -> Map {
-    let mut updated_map : Map = world.clone();
+fn update_the_map(world: &Map) -> Map {
+    let mut updated_map: Map = world.clone();
 
     let mut idx_x = 0;
     for line in world.iter() {
         let mut idx_y = 0;
         //print!("|");
         for case in line.iter() {
-  //          case.you_see(slice_map(world, case.perception, idx_x, idx_y));
+            //          case.you_see(slice_map(world, case.perception, idx_x, idx_y));
             let mut neighborhood = 0;
             if world[if_edge_else(idx_x, MAX_X)][if_edge_else(idx_y, MAX_Y)] == FULL {
                 neighborhood += 1;
@@ -94,29 +101,31 @@ fn update_the_map(world : &Map) -> Map {
                 updated_map[idx_x][idx_y] = EMPTY;
             } else if neighborhood > 3 {
                 updated_map[idx_x][idx_y] = EMPTY;
-            } else /*if neighborhood == 3*/  {
+            } else
+            /*if neighborhood == 3*/
+            {
                 updated_map[idx_x][idx_y] = FULL;
             }
             //print!("{}", neighborhood);
             idx_y += 1;
         }
         //println!("|");
-        idx_x +=1
+        idx_x += 1
     }
     updated_map
 }
 
 fn jeu_de_la_vie() {
-    let mut world: Map  = [[EMPTY;MAX_Y];MAX_X];
+    let mut world: Map = [[EMPTY; MAX_Y]; MAX_X];
     println!("Hello, world!");
 
     let mut iteration_count: u32 = 0;
 
     world[MID_X_INDEX][MID_Y_INDEX + 1] = FULL;
-    world[MID_X_INDEX+1][MID_Y_INDEX + 1] = FULL;
-    world[MID_X_INDEX+1][MID_Y_INDEX] = FULL;
-    world[MID_X_INDEX+1][MID_Y_INDEX - 1] = FULL;
-    world[MID_X_INDEX-1][MID_Y_INDEX - 1] = FULL;
+    world[MID_X_INDEX + 1][MID_Y_INDEX + 1] = FULL;
+    world[MID_X_INDEX + 1][MID_Y_INDEX] = FULL;
+    world[MID_X_INDEX + 1][MID_Y_INDEX - 1] = FULL;
+    world[MID_X_INDEX - 1][MID_Y_INDEX - 1] = FULL;
 
     while iteration_count < MAX_ITERATION {
         iteration_count += 1;
